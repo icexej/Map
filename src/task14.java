e24b0b0b75c6bf6fba988f4b72c5e48be986901b
User <alymbekovaaisulu6a@gmail.com>
2026-02-11 11:18:08 +0600

    14

---
+++ src/task14.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class task14 {
+    public static void main(String[] args) {
+        String[] words = {"hi", "book", "java", "sun", "loop", "map"};
+        HashMap<Integer, ArrayList<String>> groups = new HashMap<>();
+
+        for (String w : words) {
+            groups.computeIfAbsent(w.length(), k -> new ArrayList<>()).add(w);
+        }
+        System.out.println("Группировка по длине: " + groups);
+    }
+}
