aaeba98b4549301a4ca0cc5e2e5f02a22c8e4089
User <alymbekovaaisulu6a@gmail.com>
2026-02-11 10:59:45 +0600

    01

--- src/task1.java
+++ src/task1.java
@@ -5,7 +5,7 @@ public class task1 {
         HashMap<String, Integer> scores = new HashMap<>();
         scores.put("Aida", 85);
         scores.put("Azat", 92);
-        scores.put("Dana", 77)
+        scores.put("Dana", 77);
         System.out.println("Students: " + scores);
         System.out.println("Size: " + scores.size());
     }
