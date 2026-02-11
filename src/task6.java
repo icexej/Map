f6d58f17db5c153e6eba51f3cafa0bc33188fcde
User <alymbekovaaisulu6a@gmail.com>
2026-02-11 10:58:59 +0600

    6

---
+++ src/task6.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class task6 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> scores = new HashMap<>();
+        scores.put("Aida", 85);
+
+        int score = scores.getOrDefault("Mira", -1);
+        if (score == -1) System.out.println("Not found");
+        else System.out.println("Score: " + score);
+    }
+}
