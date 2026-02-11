0da13103ae3ec0123510c8dddb7677f6aa81e877
User <alymbekovaaisulu6a@gmail.com>
2026-02-11 10:56:50 +0600

    4

---
+++ src/task4.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class task4 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> scores = new HashMap<>();
+        scores.put("Azat", 92);
+
+        System.out.println("Remove Azat: " + scores.remove("Azat"));
+        System.out.println("Remove NonExisting: " + scores.remove("NonExisting"));
+        System.out.println("After removing: " + scores);
+    }
+}
