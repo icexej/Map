42da2e912a9448e05a5070c9e64345719f893116
User <alymbekovaaisulu6a@gmail.com>
2026-02-11 11:01:58 +0600

    02

--- src/task02.java
+++ src/task02.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class task2 {
+public class task02 {
     public static void main(String[] args) {
         HashMap<String, Integer> scores = new HashMap<>();
         scores.put("Aida", 85);
